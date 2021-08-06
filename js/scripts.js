834035bfdf86c0a911f03905ee043793b4702d67
CasasPIERO <pierorenatocasascalderon@gmail.com>
2021-08-06 14:57:48 -0500

    Adding the Equipment Section

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
         const btnMenu = document.getElementById('btnMenu');
         const navegacion = document.getElementById('navegacion');
         const icon = document.getElementById('icon');
+
         btnMenu.addEventListener('click', function(){
             navegacion.classList.toggle('nav-navegacion-visible');
             if(navegacion.classList.contains('nav-navegacion-visible')){
@@ -18,5 +19,23 @@
         }); 
         // FIN MENU HAMBURGUESA
         
+
+        // INICIO EFECTOS SECCION EQUIPO //
+        const btnTeam1 = document.getElementById('btn-team1');
+        const btnTeam2 = document.getElementById('btn-team2');
+        const team1 = document.getElementById('team1');
+        const team2 = document.getElementById('team2');
+
+        btnTeam1.addEventListener('click', function(){
+            if(team1.classList.contains('cards-none')){
+                team2.classList.add('cards-none');
+                team1.classList.remove('cards-none');
+            }
+        });
+        btnTeam2.addEventListener('click', function(){
+            team2.classList.remove('cards-none');
+            team1.classList.add('cards-none');
+        });
+        // FIN EFECTOS SECCION EQUIPO //
     });
 })();
